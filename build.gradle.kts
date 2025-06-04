25d85fb4ab70338b8fc56da797ad6ea6154ac51b
BeepBot99 <74377508+BeepBot99@users.noreply.github.com>
2025-06-04 12:44:45 -0500

    chore: set up MavenDeployer
    - Add Dokka (Maven Central requires a javadoc.jar)
    - Set up MavenDeployer with three deploy configurations:
      - Maven Local
      - Maven Central SNAPSHOT
      - Maven Central

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,11 @@ plugins {
     kotlin("jvm") version libs.versions.kotlin.get()
     `java-library`
     alias(libs.plugins.deployer)
+    alias(libs.plugins.dokka)
 }
 
 group = "dev.nextftc"
-version = "1.0.0-SNAPSHOT"
+version = property("version") as String
 
 repositories {
     mavenCentral()
@@ -19,6 +20,59 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
     jvmToolchain(17)
+}
+
+val dokkaJar = tasks.register<Jar>("dokkaJar") {
+    dependsOn(tasks.named("dokkaGenerate"))
+    from(dokka.basePublicationsDirectory)
+    archiveClassifier = "html-docs"
+}
+
+deployer {
+    projectInfo {
+        name = "NextBindings"
+        description = "A user-friendly bindings library for the FIRST Tech Challenge"
+        url = "https://nextftc.dev/bindings"
+        scm {
+            fromGithub("NextFTC", "NextBindings")
+        }
+        license("GNU General Public License, version 3", "https://www.gnu.org/licenses/gpl-3.0.html")
+        developer("Davis Luxenberg", "davis.luxenberg@outlook.com", url = "https://github.com/BeepBot99")
+    }
+
+    signing {
+        key = secret("MVN_GPG_KEY")
+        password = secret("MVN_GPG_PASSWORD")
+    }
+
+    content {
+        kotlinComponents {
+            kotlinSources()
+            docs(dokkaJar)
+        }
+    }
+
+    localSpec {
+        release.version = "$version-LOCAL"
+    }
+
+    nexusSpec("snapshot") {
+        release.version = "$version-SNAPSHOT"
+        repositoryUrl = "https://central.sonatype.com/repository/maven-snapshots/"
+        auth {
+            user = secret("SONATYPE_USERNAME")
+            password = secret("SONATYPE_PASSWORD")
+        }
+    }
+
+    centralPortalSpec {
+        auth {
+            user = secret("SONATYPE_USERNAME")
+            password = secret("SONATYPE_PASSWORD")
+        }
+        allowMavenCentralSync = false
+    }
 }
